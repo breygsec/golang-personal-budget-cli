d9d6ed02f95079237f9b74f796aea0e9b1452956
{{ .user }} <{{ .email }}>
2021-02-12 15:40:11 +0000

    fix: there was a missing '=' :sadface:

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -81,7 +81,7 @@ func (b *Budget) RemoveItem(description string) {
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
 
-	if len(report) > 12 {
+	if len(report) >= 12 {
 		return nil, errReportIsFull
 	}
 
